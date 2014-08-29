8c80bdd8de988bd82ca050a2442da4d412f715d9
nfguerrero <nicg987@gmail.com>
2014-08-29 11:32:12 -0500

    ConcatDemo added
    Added the pair program for learning to concat

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -5,7 +5,10 @@ public class TurtleWorld
     public static void main(String args[])
     {
         World turtleWorld = new World();
+        World turtleWorld2 = new World();
         Turtle turtle = new Turtle(turtleWorld);
+        Turtle turtle2 = new Turtle(turtleWorld2);
+        turtle2.forward(30);
         turtle.setPenColor(Color.RED);
         turtle.penDown();
         turtle.forward(50);
